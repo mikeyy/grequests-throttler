258d0d153796d63199995f85d9546ce24508381d
Michael Mooney <mikeyy@mikeyy.com>
2018-06-02 03:30:10 -0500

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,9 @@ from throttler import ThrottledSession, Strategy
 class HttpClient(object):
 
     def __init__(self, proxy=None, timeout=30, retries=0, headers={}):
-        self.session = ThrottledSession(delay_per_request=1, retries=retries, strategy=Strategy.Backoff())
+        self.session = ThrottledSession(delay_per_request=1,
+                                        retries=retries,
+                                        strategy=Strategy.Backoff())
         self.headers = headers
         self.timeout = timeout
 
